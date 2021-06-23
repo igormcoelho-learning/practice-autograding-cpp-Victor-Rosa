20f3906e6f2fc9bbdd89694006f44aae9b2ef991
Victor Rosa <victorsouzarosa100@gmail.com>
2021-06-23 20:56:41 -0300

    Update student.hpp

--- student.hpp
+++ student.hpp
@@ -1,6 +1,6 @@
 #pragma once // prevents multiple definitions
 
-#include <iostream> // print library
+#include <iostream> // print library 
 
 // Exercise 1: implements function 'add', that adds two numbers
 int add(int a, int b);
@@ -10,14 +10,39 @@ int mul(int a, int b);
 
 // ===============
 // implementations
+#include <stdio.h>
+#include <stdlib.h>
 
-int add(int a, int b)
-{
-   return 0; // TODO: fix
+#pragma once // prevents multiple definitions
+
+
+int add(int a, int b){
+    int resultado;
+    resultado = a * b;
+    return (resultado);
+};
+
+
+int mul(int a, int b){
+    int resultado;
+    resultado = a + b;
+    return (resultado);
+
+};
+
+int main(){
+    int num1, num2, result1, result2;
+    scanf("%d", &num1);
+    scanf("%d", &num2);
+
+    result1 = mul(num1, num2);
+    result2 = add(num1, num2);
+
+    printf("\n Resultado da multiplicacao: \t %d", result1);
+    printf("\n Resultado da adicao: \t %d", result2);
+
+    return 0;
+
 }
 
-// Exercise 2: implements function 'mul', that multiplies two numbers
-int mul(int a, int b)
-{
-   return 0; // TODO: fix
-}
+
